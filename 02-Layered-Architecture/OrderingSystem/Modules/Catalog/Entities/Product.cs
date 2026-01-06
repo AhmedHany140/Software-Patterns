3621c46a227145bb0fb1e320e700bbdf2f3d6ca0
ahmed <ahmed@gmail.com>
2026-01-06 14:32:49 +0200

    Remove 02-Layered-Architecture from git tracking

--- 02-Layered-Architecture/OrderingSystem/Modules/Catalog/Entities/Product.cs
+++
@@ -1,9 +0,0 @@
-namespace OrderingSystem.Modules.Catalog.Entities;
-
-public class Product
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public decimal Price { get; set; }
-    public int StockQuantity { get; set; }
-}
